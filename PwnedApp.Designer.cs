267a0e9b5a5328b64c38ec0cd4714042dccbe7a9
Damian Swan AAHS <damianmswan@icloud.com>
2024-06-29 19:46:40 -0400

    Add project files.

---
+++ PwnedApp.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Pwned
+{
+    partial class PwnedApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PwnedApp));
+            this.passInput = new System.Windows.Forms.TextBox();
+            this.passVistoggle = new System.Windows.Forms.Button();
+            this.passGen = new System.Windows.Forms.TextBox();
+            this.passGenS = new System.Windows.Forms.Button();
+            this.passGenlength = new System.Windows.Forms.TrackBar();
+            this.passGencomplex = new System.Windows.Forms.TrackBar();
+            this.passStyleholder = new System.Windows.Forms.GroupBox();
+            this.passStylePhrase = new System.Windows.Forms.RadioButton();
+            this.passStyleRan = new System.Windows.Forms.RadioButton();
+            this.checkPwn = new System.Windows.Forms.Button();
+            this.statusHolder = new System.Windows.Forms.GroupBox();
+            this.statusSecure = new System.Windows.Forms.TextBox();
+            this.statusPwned = new System.Windows.Forms.TextBox();
+            this.statusLab = new System.Windows.Forms.Label();
+            this.statusPoint = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.passGenlength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passGencomplex)).BeginInit();
+            this.passStyleholder.SuspendLayout();
+            this.statusHolder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // passInput
+            // 
+            this.passInput.BackColor = System.Drawing.SystemColors.Control;
+            this.passInput.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.passInput.HideSelection = false;
+            this.passInput.Location = new System.Drawing.Point(12, 12);
+            this.passInput.Name = "passInput";
+            this.passInput.PasswordChar = '*';
+            this.passInput.Size = new System.Drawing.Size(336, 20);
+            this.passInput.TabIndex = 0;
+            this.passInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // passVistoggle
+            // 
+            this.passVistoggle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.passVistoggle.Font = new System.Drawing.Font("Wingdings 2", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.passVistoggle.Location = new System.Drawing.Point(354, 10);
+            this.passVistoggle.Name = "passVistoggle";
+            this.passVistoggle.Size = new System.Drawing.Size(23, 23);
+            this.passVistoggle.TabIndex = 1;
+            this.passVistoggle.Text = "Ù";
+            this.passVistoggle.UseVisualStyleBackColor = true;
+            this.passVistoggle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passGen
+            // 
+            this.passGen.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.passGen.Location = new System.Drawing.Point(12, 39);
+            this.passGen.Name = "passGen";
+            this.passGen.ReadOnly = true;
+            this.passGen.Size = new System.Drawing.Size(335, 20);
+            this.passGen.TabIndex = 2;
+            this.passGen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // passGenS
+            // 
+            this.passGenS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.passGenS.Font = new System.Drawing.Font("Wingdings 2", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.passGenS.Location = new System.Drawing.Point(354, 39);
+            this.passGenS.Name = "passGenS";
+            this.passGenS.Size = new System.Drawing.Size(23, 23);
+            this.passGenS.TabIndex = 3;
+            this.passGenS.Text = "<";
+            this.passGenS.UseVisualStyleBackColor = true;
+            this.passGenS.Click += new System.EventHandler(this.passGenS_Click);
+            // 
+            // passGenlength
+            // 
+            this.passGenlength.Cursor = System.Windows.Forms.Cursors.NoMoveHoriz;
+            this.passGenlength.Location = new System.Drawing.Point(46, 65);
+            this.passGenlength.Maximum = 35;
+            this.passGenlength.Minimum = 8;
+            this.passGenlength.Name = "passGenlength";
+            this.passGenlength.Size = new System.Drawing.Size(102, 45);
+            this.passGenlength.TabIndex = 1;
+            this.passGenlength.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.passGenlength.Value = 8;
+            // 
+            // passGencomplex
+            // 
+            this.passGencomplex.Cursor = System.Windows.Forms.Cursors.NoMoveHoriz;
+            this.passGencomplex.Location = new System.Drawing.Point(154, 65);
+            this.passGencomplex.Maximum = 4;
+            this.passGencomplex.Minimum = 1;
+            this.passGencomplex.Name = "passGencomplex";
+            this.passGencomplex.Size = new System.Drawing.Size(102, 45);
+            this.passGencomplex.TabIndex = 4;
+            this.passGencomplex.Value = 1;
+            // 
+            // passStyleholder
+            // 
+            this.passStyleholder.Controls.Add(this.passStylePhrase);
+            this.passStyleholder.Controls.Add(this.passStyleRan);
+            this.passStyleholder.Location = new System.Drawing.Point(262, 65);
+            this.passStyleholder.Name = "passStyleholder";
+            this.passStyleholder.Size = new System.Drawing.Size(74, 45);
+            this.passStyleholder.TabIndex = 5;
+            this.passStyleholder.TabStop = false;
+            this.passStyleholder.Text = "passStyle";
+            // 
+            // passStylePhrase
+            // 
+            this.passStylePhrase.AutoSize = true;
+            this.passStylePhrase.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.passStylePhrase.Location = new System.Drawing.Point(45, 19);
+            this.passStylePhrase.Name = "passStylePhrase";
+            this.passStylePhrase.Size = new System.Drawing.Size(14, 13);
+            this.passStylePhrase.TabIndex = 1;
+            this.passStylePhrase.UseVisualStyleBackColor = true;
+            this.passStylePhrase.CheckedChanged += new System.EventHandler(this.passStylePhrase_CheckedChanged);
+            // 
+            // passStyleRan
+            // 
+            this.passStyleRan.AutoSize = true;
+            this.passStyleRan.Checked = true;
+            this.passStyleRan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.passStyleRan.Location = new System.Drawing.Point(12, 19);
+            this.passStyleRan.Name = "passStyleRan";
+            this.passStyleRan.Size = new System.Drawing.Size(14, 13);
+            this.passStyleRan.TabIndex = 0;
+            this.passStyleRan.TabStop = true;
+            this.passStyleRan.UseVisualStyleBackColor = true;
+            this.passStyleRan.CheckedChanged += new System.EventHandler(this.passStyleRan_CheckedChanged);
+            // 
+            // checkPwn
+            // 
+            this.checkPwn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkPwn.Location = new System.Drawing.Point(108, 116);
+            this.checkPwn.Name = "checkPwn";
+            this.checkPwn.Size = new System.Drawing.Size(101, 23);
+            this.checkPwn.TabIndex = 6;
+            this.checkPwn.Text = "Am I \'Pwned";
+            this.checkPwn.UseVisualStyleBackColor = true;
+            this.checkPwn.Click += new System.EventHandler(this.checkPwn_Click);
+            // 
+            // statusHolder
+            // 
+            this.statusHolder.Controls.Add(this.statusSecure);
+            this.statusHolder.Controls.Add(this.statusPwned);
+            this.statusHolder.Location = new System.Drawing.Point(343, 69);
+            this.statusHolder.Name = "statusHolder";
+            this.statusHolder.Size = new System.Drawing.Size(57, 89);
+            this.statusHolder.TabIndex = 7;
+            this.statusHolder.TabStop = false;
+            this.statusHolder.Text = "Status";
+            // 
+            // statusSecure
+            // 
+            this.statusSecure.BackColor = System.Drawing.Color.Beige;
+            this.statusSecure.Enabled = false;
+            this.statusSecure.Font = new System.Drawing.Font("Wingdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.statusSecure.Location = new System.Drawing.Point(17, 52);
+            this.statusSecure.Name = "statusSecure";
+            this.statusSecure.Size = new System.Drawing.Size(23, 25);
+            this.statusSecure.TabIndex = 1;
+            this.statusSecure.Text = "¹";
+            this.statusSecure.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // statusPwned
+            // 
+            this.statusPwned.BackColor = System.Drawing.Color.Beige;
+            this.statusPwned.Enabled = false;
+            this.statusPwned.Font = new System.Drawing.Font("Wingdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.statusPwned.Location = new System.Drawing.Point(17, 15);
+            this.statusPwned.Name = "statusPwned";
+            this.statusPwned.Size = new System.Drawing.Size(23, 25);
+            this.statusPwned.TabIndex = 0;
+            this.statusPwned.Text = "¹";
+            this.statusPwned.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // statusLab
+            // 
+            this.statusLab.AutoSize = true;
+            this.statusLab.Location = new System.Drawing.Point(259, 113);
+            this.statusLab.Name = "statusLab";
+            this.statusLab.Size = new System.Drawing.Size(50, 39);
+            this.statusLab.TabIndex = 8;
+            this.statusLab.Text = "\'Pwned\r\n\r\nSecure?\r\n";
+            // 
+            // statusPoint
+            // 
+            this.statusPoint.AutoSize = true;
+            this.statusPoint.Font = new System.Drawing.Font("Wingdings", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.statusPoint.Location = new System.Drawing.Point(315, 116);
+            this.statusPoint.Name = "statusPoint";
+            this.statusPoint.Size = new System.Drawing.Size(17, 36);
+            this.statusPoint.TabIndex = 9;
+            this.statusPoint.Text = "è\r\n\r\nè\r\n";
+            // 
+            // PwnedApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(404, 163);
+            this.Controls.Add(this.statusPoint);
+            this.Controls.Add(this.statusLab);
+            this.Controls.Add(this.statusHolder);
+            this.Controls.Add(this.checkPwn);
+            this.Controls.Add(this.passStyleholder);
+            this.Controls.Add(this.passGencomplex);
+            this.Controls.Add(this.passGenlength);
+            this.Controls.Add(this.passGenS);
+            this.Controls.Add(this.passGen);
+            this.Controls.Add(this.passVistoggle);
+            this.Controls.Add(this.passInput);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "PwnedApp";
+            this.Text = "Pwned";
+            this.Load += new System.EventHandler(this.PwnedApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.passGenlength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passGencomplex)).EndInit();
+            this.passStyleholder.ResumeLayout(false);
+            this.passStyleholder.PerformLayout();
+            this.statusHolder.ResumeLayout(false);
+            this.statusHolder.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox passInput;
+        private System.Windows.Forms.Button passVistoggle;
+        private System.Windows.Forms.TextBox passGen;
+        private System.Windows.Forms.Button passGenS;
+        private System.Windows.Forms.TrackBar passGenlength;
+        private System.Windows.Forms.TrackBar passGencomplex;
+        private System.Windows.Forms.GroupBox passStyleholder;
+        private System.Windows.Forms.RadioButton passStyleRan;
+        private System.Windows.Forms.RadioButton passStylePhrase;
+        private System.Windows.Forms.Button checkPwn;
+        private System.Windows.Forms.GroupBox statusHolder;
+        private System.Windows.Forms.Label statusLab;
+        private System.Windows.Forms.Label statusPoint;
+        private System.Windows.Forms.TextBox statusPwned;
+        private System.Windows.Forms.TextBox statusSecure;
+    }
+}
+
---
+++ PwnedApp.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+
+namespace Pwned
+{
+    public partial class PwnedApp : Form
+    {
+        public PwnedApp()
+        {
+            InitializeComponent();
+        }
+
+        private void PwnedApp_Load(object sender, EventArgs e)
+        {
+            passVistoggle.BackColor = Color.White;
+            passGenlength.Minimum = 8;
+            passGenlength.Maximum = 35;
+            statusPwned.BackColor = Color.Beige;
+            statusSecure.BackColor = Color.Beige;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            passInput.PasswordChar = passInput.PasswordChar == '*' ? '\0' : '*';
+            if (passVistoggle.BackColor == Color.White)
+            {
+                passVistoggle.BackColor = Color.Yellow;
+            }
+            else
+            {
+                passVistoggle.BackColor = Color.White;
+            }
+        }
+
+        private void passGenS_Click(object sender, EventArgs e)
+        {
+            int passwordLength = passGenlength.Value;
+            int complexity = passGencomplex.Value;
+            string password = "";
+
+            if (passStyleRan.Checked)
+            {
+                password = GenerateRandomPassword(passwordLength, complexity);
+            }
+            else
+            {
+                password = GenerateWordPassword(passwordLength, complexity);
+            }
+
+            passGen.Text = password;
+        }
+
+        private string GenerateRandomPassword(int length, int complexity)
+        {
+            string uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+            string numericChars = "0123456789";
+            string specialChars = "!@#$%^&*()_+";
+
+            string allChars = "";
+
+            // Add character sets based on complexity level
+            if (complexity >= 1)
+            {
+                allChars += uppercaseChars;
+            }
+            if (complexity >= 2)
+            {
+                allChars += lowercaseChars;
+            }
+            if (complexity >= 3)
+            {
+                allChars += numericChars;
+            }
+            if (complexity >= 4)
+            {
+                allChars += specialChars;
+            }
+
+            Random random = new Random();
+            string password = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(0, allChars.Length);
+                password += allChars[index];
+            }
+
+            return password;
+        }
+
+        private string GenerateWordPassword(int length, int complexity)
+        {
+            string[] words = { "apple", "banana", "carrot", "dog", "elephant", "flower", "grape", "honey", "ice cream", "jungle", "kiwi", "lemon", "mango", "nut", "orange", "pear", "quince", "raspberry", "strawberry", "tomato" };
+            string[] symbols = { "@", "#", "$", "%", "&", "*", "+", "-", "=", "?", "!" };
+
+            Random random = new Random();
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                int wordIndex = random.Next(0, words.Length);
+                int symbolIndex = random.Next(0, symbols.Length);
+
+                passwordBuilder.Append(words[wordIndex]);
+                passwordBuilder.Append(symbols[symbolIndex]);
+
+
+            }
+
+            return passwordBuilder.ToString();
+        }
+
+        private void passStyleRan_CheckedChanged(object sender, EventArgs e)
+        {
+            passGenlength.Minimum = 8;
+            passGenlength.Maximum = 35;
+            passGenlength.Value = 8;
+        }
+
+        private void passStylePhrase_CheckedChanged(object sender, EventArgs e)
+        {
+            passGenlength.Minimum = 4;
+            passGenlength.Maximum = 10;
+            passGenlength.Value = 4;
+        }
+
+        private async void checkPwn_Click(object sender, EventArgs e)
+        {
+            string hashPass = HashGenerator.ToSHA1(passInput.Text);
+            Console.WriteLine("Hashed Passkey is: " + hashPass);
+            string hashKey = hashPass.Substring(0, 5);
+            string hashFall = hashPass.Substring(5);
+            Console.WriteLine("Fallback: " + hashFall);
+            Console.WriteLine("Hash Key to Post is: " + hashKey);
+
+            await CheckPasswordPwnedAsync(hashKey, hashFall);
+            await CalculatePasswordComplexityAsync();
+        }
+        private async Task CheckPasswordPwnedAsync(string hashKey, string hashPass)
+        {
+            string url = "https://api.pwnedpasswords.com/range/" + hashKey;
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            string content = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string[] hashes = content.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                bool isPwned = false;
+                int count = 0;
+                foreach (string hash in hashes)
+                {
+                    string[] parts = hash.Split(':');
+                    // Compare the hash suffix from the API with hashPass, ensuring case-insensitive comparison
+                    if (string.Equals(parts[0], hashPass, StringComparison.OrdinalIgnoreCase))
+                    {
+                        isPwned = true;
+                        count = int.Parse(parts[1]);
+                        break;
+                    }
+                }
+
+                if (isPwned)
+                {
+                    statusPwned.Text = "ý";
+                    statusPwned.BackColor = Color.Crimson;
+                    MessageBox.Show($"Password has been pwned {count} time(s)!");
+                }
+                else
+                {
+                    statusPwned.Text = "þ";
+                    statusPwned.BackColor = Color.LightGreen;
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Error checking password: {response.StatusCode}");
+            }
+        }
+        private async Task CalculatePasswordComplexityAsync()
+        {
+            string password = passInput.Text;
+            int complexity = 0;
+            int taskcounter = 0;
+
+            // Check for uppercase letters
+            if (Regex.IsMatch(password, "[A-Z]"))
+            {
+                complexity += Regex.Matches(password, "[A-Z]").Count;
+                taskcounter++;
+            }
+
+            // Check for lowercase letters
+            if (Regex.IsMatch(password, "[a-z]"))
+            {
+                complexity += Regex.Matches(password, "[a-z]").Count;
+                taskcounter++;
+            }
+
+            // Check for numeric characters
+            if (Regex.IsMatch(password, "[0-9]"))
+            {
+                complexity += Regex.Matches(password, "[0-9]").Count;
+                taskcounter++;
+            }
+
+            // Check for special characters
+            if (Regex.IsMatch(password, "[!@#$%^&*()_+]"))
+            {
+                complexity += Regex.Matches(password, "[!@#$%^&*()_+]").Count;
+                taskcounter++;
+            }
+
+            Console.WriteLine("COM1:" + complexity);
+            // Check for consecutive or progressive characters
+            int ogcomplex = complexity;
+            complexity -= CountConsecutiveCharacters(password);
+            complexity -= CountProgressiveCharacters(password);
+            Console.WriteLine("COM2:" + complexity);
+            // Calculate the time to crack the password
+            double timeToCrack = Math.Pow(26, password.Length) / 2;
+            if (statusPwned.Text == "ý")
+            {
+                complexity = 1;
+                taskcounter = 1;
+            }
+            else
+            {
+                switch (true)
+                {
+                    case var count when CountConsecutiveCharacters(password) > 3:
+                        int rep = CountConsecutiveCharacters(password);
+                        complexity -= rep;
+                        taskcounter--;
+                        break;
+                    case var count when CountProgressiveCharacters(password) > 3:
+                        int prog = CountProgressiveCharacters(password);
+                        complexity -= prog;
+                        taskcounter--;
+                        break;
+                }
+            }
+            Console.WriteLine("TSK:" + taskcounter);
+            // Display the complexity and time to crack
+            Console.WriteLine($"Password Complexity: {complexity}");
+            if (complexity < 9)
+            {
+                statusSecure.BackColor = Color.Crimson;
+                statusSecure.Text = "ý";
+            }
+            else if (complexity >= 9 && taskcounter < 3)
+            {
+                statusSecure.BackColor = Color.LightYellow;
+                statusSecure.Text = "þ";
+                MessageBox.Show("Password seems secure, but dosen't meet all the recommended complexity requirements!\n" +
+                    "Such as:\n" +
+                    "- At least 8 Characters\n" +
+                    "- At least one Uppercase and Lowercase character\n" +
+                    "- At least one Number\n" +
+                    "- At least one Symbol");
+            }
+            else { statusSecure.BackColor = Color.LightGreen; statusSecure.Text = "þ"; }
+        }
+
+        private int CountConsecutiveCharacters(string password)
+        {
+            int count = 0;
+            char previousChar = '\0'; // Initialize previous character with null character
+
+            foreach (char currentChar in password)
+            {
+                if (previousChar != '\0' && (currentChar == previousChar || currentChar == previousChar + 1 || currentChar == previousChar - 1))
+                {
+                    count++;
+                    Console.WriteLine("CONS:" + count);
+                }
+
+                previousChar = currentChar;
+            }
+
+            return count;
+        }
+
+        private int CountProgressiveCharacters(string password)
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length - 2; i++)
+            {
+                if (password[i] + 1 == password[i + 1] && password[i + 1] + 1 == password[i + 2])
+                {
+                    count++;
+                    Console.WriteLine("PROG:" + count);
+                }
+            }
+            return count;
+        }
+        private string[] ReadArrayFromFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            return lines;
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // Check if the Enter key is pressed
+            if (keyData == Keys.Enter)
+            {
+                // Trigger the checkPwn_Click event programmatically
+                // Pass 'null' for both sender and EventArgs since they are not used in the method
+                checkPwn_Click(null, null);
+
+                // Return true to indicate that the key press has been handled
+                return true;
+            }
+
+            // Call the base class method to handle other key presses
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        /*
+        private async void passInput_TextChanged(object sender, EventArgs e)
+        {
+            // Create a TaskCompletionSource to track when the TextBox is unfocused
+            TaskCompletionSource<bool> unfocusedTask = new TaskCompletionSource<bool>();
+
+
+            // Attach an event handler to the TextBox's Leave event
+            passInput.Leave += (leaveSender, leaveArgs) =>
+            {
+                // Set the TaskCompletionSource result to true when the TextBox is unfocused
+                unfocusedTask.SetResult(true);
+            };
+
+            // Wait until the TextBox is unfocused
+            await unfocusedTask.Task;
+
+            // Trigger the checkPwn_Click event
+            checkPwn_Click(sender, e);
+        }
+        */
+    }
+
+
+
+    public class HashGenerator
+    {
+        public static string ToSHA1(string text)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] textBytes = Encoding.UTF8.GetBytes(text);
+                byte[] hashBytes = sha1.ComputeHash(textBytes);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.AppendFormat("{0:x2}", b);
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
